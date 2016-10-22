eea022e546ed8791b82f8e53ecdd4a14afe330f0
chekn <chekn@3FCD3B5572-PC>
2016-10-22 10:55:18 +0800

    syn error fix

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 
 
 chrome.browserAction.onClicked.addListener(function( tab ) {
-	var jsonH_url = chrome.extension.getURL("JS-Fmt/JS-Fmt.html");
+	var jsonH_url = chrome.extension.getURL("JS-Fmt/JS_Fmt.html");
 	chrome.tabs.create({"url":jsonH_url, "selected":true});
 });

5aba5b72e832009fabe63d489fa5f7292fd94674
chekn <chekn@3FCD3B5572-PC>
2016-10-30 16:17:08 +0800

    add h5 img cvt base64 tool

--- JS-Fmt/js/init.js
+++ JS-Fmt/js/init.js
@@ -33,3 +33,37 @@ $(function() {
 		$("#valueAct").dialog("close");
 	});
 });
+
+
+var u = {
+		  img: function (e) {
+		    if (!e) return ;
+		    var gbi = function (img) {
+		      var canvas = document.createElement('canvas');
+		      canvas.width = img.width;
+		      canvas.height = img.height;
+		      var ctx = canvas.getContext('2d');
+		      ctx.drawImage(img, 0, 0, img.width, img.height);
+		      var ext = img.src.substring(img.src.lastIndexOf('.') + 1) .toLowerCase();
+		      var dataURL = canvas.toDataURL('image/' + ext);
+		      return dataURL;
+		    }
+		    if (typeof (e) == 'string') {
+		      console.info(e);
+		      var image = new Image();
+		      image.crossOrigin = '';
+		      image.src = e;
+		      var x;
+		      image.onload=function() {x= gbi(image);console.info(x);}
+		    } else if (e instanceof Image) {
+		      var base64 = gbi(e);
+		      console.log(base64);
+		      return base64;
+		    } else if (e instanceof File) {
+		      var x;
+		      var fr = new FileReader();
+		      fr.onload = function () {x = this.result; console.info(x);}
+		      fr.readAsDataUrl(e);
+		    }
+		  }
+		}
